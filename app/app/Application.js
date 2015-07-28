06f7245e06074c7278f4ab618fc83a6d84619957
Chris Alfano <chris@jarv.us>
2015-07-27 20:23:32 -0400

    use raw HTTP calls to demo API

--- app/app/Application.js
+++ app/app/Application.js
@@ -9,6 +9,7 @@ Ext.define('PostgrestTest.Application', {
     requires: [
         'PostgrestTest.model.Jurisdiction',
         'PostgrestTest.store.Jurisdictions',
+        'Jarvus.connection.Postgrest',
         'Jarvus.proxy.Postgrest',
         'Jarvus.reader.Postgrest'
     ],
@@ -20,16 +21,52 @@ Ext.define('PostgrestTest.Application', {
     ],
     
     launch: function () {
-        Ext.getStore('PostgrestTest.store.Jurisdictions').load({callback: function loaded() {
-            debugger;
+        // Ext.getStore('PostgrestTest.store.Jurisdictions').load({callback: function loaded() {
+        //     debugger;
 
-            var store = Ext.getStore('PostgrestTest.store.Jurisdictions');
+        //     var store = Ext.getStore('PostgrestTest.store.Jurisdictions');
 
-            store.filter([{
-                property: 'title',
-                operator: 'in',
-                value:    ['Pennsylvania', 'New Jersey', 'Delaware']
-            }]);
-        }});
+        //     store.filter([{
+        //         property: 'title',
+        //         operator: 'in',
+        //         value:    ['Pennsylvania', 'New Jersey', 'Delaware']
+        //     }]);
+        // }});
+
+
+        // demo call: get a list of tables
+        Jarvus.connection.Postgrest.request({
+            url: '/',
+            success: function(response) {
+                var r = Ext.decode(response.responseText);
+
+                console.log('Loaded list of schemas');
+                console.table(r);
+            }
+        });
+
+        // load all jurisdictions
+        Jarvus.connection.Postgrest.request({
+            url: '/jurisdictions',
+            success: function(response) {
+                var r = Ext.decode(response.responseText);
+
+                console.log('Loaded list of jurisdictions');
+                console.table(r);
+            }
+        });
+        
+        // load one jurisdiction
+        Jarvus.connection.Postgrest.request({
+            url: '/jurisdictions',
+            urlParams: {
+                id: 'eq.3'
+            },
+            success: function(response) {
+                var r = Ext.decode(response.responseText);
+
+                console.log('Loaded one jurisdiction:', r[0]);
+            }
+        });
     }
 });
