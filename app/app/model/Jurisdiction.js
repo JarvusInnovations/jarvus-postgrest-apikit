5c9ce5f75b6e0a5b10305440d0a5f6a35774228f
Chris Alfano <chris@jarv.us>
2015-07-27 21:41:06 -0400

    use negative ID generator for phantom models

--- app/app/model/Jurisdiction.js
+++ app/app/model/Jurisdiction.js
@@ -1,5 +1,11 @@
 Ext.define('PostgrestTest.model.Jurisdiction', {
     extend: 'Ext.data.Model',
+    requires: [
+        'Ext.data.identifier.Negative'
+    ],
+
+
+    identifier: 'negative',
 
     fields: [
         { name: 'id', type: 'int', persist: false },
