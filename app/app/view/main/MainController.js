dbb131f37bed574315545e6db351232491cd8f26
Chris Alfano <chris@jarv.us>
2015-07-27 23:58:02 -0400

    removed unused starter files

--- app/app/view/main/MainController.js
+++
@@ -1,9 +0,0 @@
-Ext.define('PostgrestTest.view.main.MainController', {
-    extend: 'Ext.app.ViewController',
-
-    requires: [
-        'Ext.window.MessageBox'
-    ],
-
-    alias: 'controller.main'
-});
--- app/app/view/main/MainModel.js
+++
@@ -1,9 +0,0 @@
-Ext.define('PostgrestTest.view.main.MainModel', {
-    extend: 'Ext.app.ViewModel',
-
-    alias: 'viewmodel.main',
-
-    data: {
-        name: 'PostgrestTest'
-    }
-});
